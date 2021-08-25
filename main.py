54f4de654c638cda02b23f5502de558cedeec5ff
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-24 23:30:24 -0300

    some docs on routes

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ app = FastAPI()
 
 @app.get('/')
 def index():
+    """
+    blog index page
+    """
     return {'data': {'name': 'Afonso'}}
 
 
 @app.get('/about')
 def about():
+    """
+    blog about page
+    """
     return {'data': {'name': 'about page'}}
 
 
 @app.get('/blog/{id}')
 def show(id):
+    """
+    get blog with id = id
+    """
     return {'data': {'id': id}} 
 
 
 @app.get('/blog/{id}/comments')
 def comments(id):
+    """
+    fetch comments of blog with id = id
+    """
     return {'data': {'comments': []}}
