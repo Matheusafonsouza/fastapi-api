1a5dbac57797448c170ed26d9a625ce0272894b2
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-29 17:24:13 -0300

    update and delete blog instance

--- blog/main.py
+++ blog/main.py
@@ -31,10 +31,42 @@ def create(request: BlogSchema, db: Session = Depends(get_db)):
 
     return blog
 
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog with the id {id} does not exists.'
+        )
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: BlogSchema, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog with the id {id} does not exists.'
+        )
+    
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog')
 def create(db: Session = Depends(get_db)):
     blogs = db.query(Blog).all()
     return blogs
+
 
 @app.get('/blog/{id}')
 def create(id: int, response: Response, db: Session = Depends(get_db)):
