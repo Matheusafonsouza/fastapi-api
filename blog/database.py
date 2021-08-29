ae6ab9ab4f238888a7d5236fc3d4c3a7f0e2fa2f
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-29 17:04:25 -0300

    saving new blog on database

--- blog/database.py
+++ blog/database.py
@@ -8,5 +8,5 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
     'check_same_thread': False
 })
 
-Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI
-from blog.schemas import Blog
+from fastapi import FastAPI, Depends
+from blog.schemas import Blog as BlogSchema
 from . import models
-from blog.database import engine
+from blog.database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from blog.models import Blog
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close( )
 
 app = FastAPI()
 
 @app.post('/blog')
-def create(request: Blog):
-    return request
+def create(request: BlogSchema, db: Session = Depends(get_db)):
+    title = request.title
+    body = request.body
+
+    blog = Blog(
+        title=title,
+        body=body,
+    )
+    db.add(blog)
+    db.commit()
+    db.refresh(blog)
+
+    return blog
