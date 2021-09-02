5435d8a65a6eeb1e2f0d838bc0bb6abfc7156584
matheusafonsouza <matheusafonsouza@gmail.com>
2021-09-02 02:10:45 -0300

    user entity and routes

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,10 @@
 from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from blog.schemas import Blog as BlogSchema, ShowBlog
+from blog.schemas import Blog as BlogSchema, User as UserSchema, ShowUser, ShowBlog
 from . import models
 from blog.database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from blog.models import Blog
+from blog.models import Blog, User
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -79,3 +79,68 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
             detail=f'Blog with the id {id} does not exists.'
         )
     return blog
+
+@app.post('/user', status_code=status.HTTP_201_CREATED)
+def create(request: UserSchema, db: Session = Depends(get_db)):
+    name = request.name
+    email = request.email
+    password = request.password
+
+    user = User(
+        name=name,
+        email=email,
+        password=password,
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == id)
+
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with the id {id} does not exists.'
+        )
+    
+    user.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/user/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: UserSchema, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == id)
+
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with the id {id} does not exists.'
+        )
+    
+    user.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+@app.get('/user', response_model=List[ShowUser])
+def list_all(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+
+@app.get('/user/{id}', response_model=ShowUser)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with the id {id} does not exists.'
+        )
+    return user
