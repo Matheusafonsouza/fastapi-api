a18f9cd8cf71bdaef5c76fcae8b1039c80c4cb53
matheusafonsouza <matheusafonsouza@gmail.com>
2021-09-05 18:07:08 -0300

    using oauth2 authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,9 +2,11 @@ from blog.repositories import blog as blog_repository
 from fastapi import APIRouter
 from blog.schemas import Blog as BlogSchema, ShowBlog
 from fastapi import status, Depends
+from blog.schemas import User as UserSchema
 from sqlalchemy.orm import Session
 from typing import List
 from blog.database import get_db
+from blog.oauth2 import get_current_user
 
 
 router = APIRouter(
@@ -14,25 +16,45 @@ router = APIRouter(
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: BlogSchema, db: Session = Depends(get_db)):
+def create(
+    request: BlogSchema,
+    db: Session = Depends(get_db),
+    current_user: UserSchema = Depends(get_current_user)
+):
     return blog_repository.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
+def destroy(
+    id,
+    db: Session = Depends(get_db),
+    current_user: UserSchema = Depends(get_current_user)
+):
     return blog_repository.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: BlogSchema, db: Session = Depends(get_db)):
+def update(
+        id,
+        request: BlogSchema,
+        db: Session = Depends(get_db),
+        current_user: UserSchema = Depends(get_current_user)
+):
     return blog_repository.update(id, request, db)
 
 
 @router.get('/', response_model=List[ShowBlog])
-def get_all(db: Session = Depends(get_db)):
+def get_all(
+    db: Session = Depends(get_db),
+    current_user: UserSchema = Depends(get_current_user)
+):
     return blog_repository.get_all(db)
 
 
 @router.get('/{id}', response_model=ShowBlog)
-def show(id: int, db: Session = Depends(get_db)):
+def show(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: UserSchema = Depends(get_current_user)
+):
     return blog_repository.show(id, db)
--- blog/utils.py
+++ blog/utils.py
@@ -1,7 +1,9 @@
 from typing import Optional
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from blog.settings import SECRET_KEY, ALGORITHM
+from blog.exceptions import credentials_exception
+from blog.schemas import TokenData
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -13,3 +15,15 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
