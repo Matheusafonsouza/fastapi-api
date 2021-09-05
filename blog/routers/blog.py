242a21053fbf429f40b8e38bd425c2cbf096135a
matheusafonsouza <matheusafonsouza@gmail.com>
2021-09-04 21:30:12 -0300

    changed blog repository import

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,4 +1,4 @@
-from blog.repositories import blog
+from blog.repositories import blog as blog_repository
 from fastapi import APIRouter
 from blog.schemas import Blog as BlogSchema, ShowBlog
 from fastapi import status, Depends
@@ -15,24 +15,24 @@ router = APIRouter(
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: BlogSchema, db: Session = Depends(get_db)):
-    return blog.create(request, db)
+    return blog_repository.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    return blog.destroy(id, db)
+    return blog_repository.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: BlogSchema, db: Session = Depends(get_db)):
-    return blog.update(id, request, db)
+    return blog_repository.update(id, request, db)
 
 
 @router.get('/', response_model=List[ShowBlog])
 def get_all(db: Session = Depends(get_db)):
-    return blog.get_all(db)
+    return blog_repository.get_all(db)
 
 
 @router.get('/{id}', response_model=ShowBlog)
 def show(id: int, db: Session = Depends(get_db)):
-    return blog.show(id, db)
+    return blog_repository.show(id, db)
